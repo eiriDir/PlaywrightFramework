7ccd2d36522913f7d7f250253a8c0eee59c76798
eiriDir <dir.lumiere@gmail.com>
2024-07-30 15:12:06 +0200

    upd

--- src/main/java/org/example/enums/TextData.java
+++ src/main/java/org/example/enums/TextData.java
@@ -3,6 +3,7 @@ package org.example.enums;
 public enum TextData {
     EMAIL("eiridyr@gmail.com"),
     PASSWORD("Wg6y2"),
+    //
     BASE_URL("https://demo.solomono.net");
 
     private final String text;
